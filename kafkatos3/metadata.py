6cd1e57f70095e4a3f4511722050e6976753ce6f
Ben Corlett <ben.corlett@gmail.com>
2016-10-12 14:19:35 +0000

    Update version and project url

--- kafkatos3/metadata.py
+++ kafkatos3/metadata.py
@@ -8,11 +8,11 @@ Information describing the project.
 package = 'kafkatos3'
 project = "Kafka to S3"
 project_no_spaces = project.replace(' ', '')
-version = '0.1'
+version = '0.1.1'
 description = 'Archive kafka messages to S3'
 authors = ['Ben Corlett', 'Paul Makkar']
 authors_string = ', '.join(authors)
 emails = ['ben.corlett@bgch.co.uk', 'paul.makkar@bgch.co.uk']
 license = 'MIT'
 copyright = '2016 ' + authors_string
-url = 'http://example.com/'
+url = 'https://github.com/ConnectedHomes/kafkatos3'
